0ae3597fa12f544e2fde0f76f5223532cd578fc1
Maria Dalila Vieira <mdadavivi@gmail.com>
2020-01-23 01:24:58 -0300

    criadas variaveis

--- placement/verilog/placement.v
+++ placement/verilog/placement.v
@@ -1,7 +1,20 @@
-module placement(clk, n, n_node, n_edge, size_offset);
+module placement(clk);
 	input clk;
-	input wire [31:0] n;
-	input wire [7:0] n_node, n_edge, size_offset;
+	//COLOCAR COMO parameter(constante recebida)
+	parameter n = 4, n_node = 14, n_edge = 15, size_offset = 28;
+
+	reg [0:2] state;
+	reg [0:63] i, j;
+	reg [0:63] pos_a_X, pos_a_Y, pos_b_X, pos_b_Y;	
+	reg [0:63] a, b;
+	reg [0:63] xi, xj;
+
+	reg [0:63] sum = 0;
+	reg [0:63] diff_pos_x, diff_pos_y;	
+	reg [0:n_edge] e_a, e_b;
+	reg [0:n_node] pos_X, pos_Y;
+	reg [0:n*n] grid;
+	reg [0:size_offset] offset_x, offset_y;
 
 
 endmodule
@@ -10,10 +23,10 @@ module test;
 	/* Make a regular pulsing clock. */
 	reg clk = 0;
 	always #5 clk = !clk;
-	parameter n = 9;
-	wire [7:0] n_node;
-	wire [7:0] n_edge;
-	wire [7:0] size_offset;
+	
+	//wire [7:0] n_node;
+	//wire [7:0] n_edge;
+	//wire [7:0] size_offset;
 
-	placement p1 (clk, n, n_node, n_edge, size_offset);
+	placement p1 (clk);
 endmodule // test
