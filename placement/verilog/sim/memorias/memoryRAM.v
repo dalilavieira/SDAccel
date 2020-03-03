51d0cf20a652e88b14b3e86bf7578c15fece1e4c
Maria Dalila Vieira <mdadavivi@gmail.com>
2020-03-03 17:39:31 -0300

    remove reset da memoriaRAM

--- placement/verilog/sim/memorias/memoryRAM.v
+++ placement/verilog/sim/memorias/memoryRAM.v
@@ -1,30 +1,32 @@
 module memoryRAM #(
   parameter init_file = "mem_file.txt",
   parameter data_depth = 4
-  )(clk, read, write, addr, dataRead, dataWrite);
-  input clk, read, write;
+  )(clk, reset, read, write, addr, dataRead, dataWrite);
+  input clk, reset, read, write;
   input signed [32-1:0] addr, dataWrite;
   output reg signed [32-1:0] dataRead;
   reg signed [32-1:0] memRAM [(2**data_depth)-1:0];
   reg [32-1:0] i, j;
   always @ (posedge clk) begin
-    dataRead <= 0;
-    if(read) begin
-      dataRead <= memRAM[addr];
-    end
-    if(write) begin
-      memRAM[addr] <= dataWrite;
-    end
-    $display();
-    for(i=0; i<data_depth; i++) begin
-			for(j=0; j<data_depth; j++) begin
-				$write("%4d", $signed(memRAM[i*data_depth+j]));
-			end
-		  $display();
-    end
-  end
-  initial begin
-    dataRead = 0;
-    $readmemh(init_file, memRAM, 0, (2**data_depth)-1);
+	if(reset) begin
+		dataRead = 0;
+    		$readmemh(init_file, memRAM, 0, (2**data_depth)-1);
+	end
+	else begin
+	    dataRead <= 0;
+	    if(read) begin
+	      dataRead <= memRAM[addr];
+	    end
+	    if(write) begin
+	      memRAM[addr] <= dataWrite;
+	    end
+	    $display();
+	    for(i=0; i<data_depth; i++) begin
+		for(j=0; j<data_depth; j++) begin
+		    $write("%4d", $signed(memRAM[i*data_depth+j]));
+		end
+		$display();
+	    end
+	end
   end
 endmodule
