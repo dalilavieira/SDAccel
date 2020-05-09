47b32e069ff2ff6691a07c6502c4249e3151e02d
Maria Dalila Vieira <mdadavivi@gmail.com>
2020-05-08 23:01:54 -0300

    .

---
+++ routing/LegUp/setup_quartus_env.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export QUARTUS_HOME=/opt/opt/altera_pro/16.0.0/quartus
+export LM_LICENSE_FILE=27000@localhost
+export PATH=$PATH:/opt/altera_pro/16.0.0/quartus/bin
+
+#export PATH=$PATH:/opt/synopsys/vcs/bin
+#export SNPSLMD_LICENSE_FILE=27000@200.131.223.215
+#export VCS_HOME=/opt/synopsys/vcs
+#export VCS_ARCH_OVERRIDE=linux
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/lib:/lib:/opt/aalsdk/lib
+#export ALLSDK_BASEDIR=/opt/intelFPGA/aalsdk
+#export OPAE_BASEDIR=/opt/intelFPGA/opae-sdk
+#export OPAE_PLATFORM_ROOT=/opt/intelFPGA/aalsdk
+#export FPGA_BBB_CCI_SRC=/opt/intelFPGA/intel-fpga-bbb
+#export FAM_BASEDIR=/home/lucas/Documentos/acc-management-repo
